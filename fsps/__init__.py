9d002c3b599011a8b5965113c2cb08aa84e36401
Dan F-M <danfm@nyu.edu>
2012-04-19 11:27:15 -0400

    added a few more environment variable checks

--- fsps/__init__.py
+++ fsps/__init__.py
@@ -1,3 +1,14 @@
+import os
+
+try:
+    ev = os.environ["SPS_HOME"]
+except KeyError:
+    raise ImportError("You need to have the SPS_HOME environment variable")
+
+expected = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fsps")
+if ev != expected:
+    print "Warning: $SPS_HOME is set to %s. Expected %s."%(ev, expected)
+
 from _fsps import fsps
 
 # Because I don't know Fortran all that well, this is how I hack things to

3da466e1001379d71ef0d3c8f4d2454ea9780197
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-05-11 08:54:25 -0700

    working FSPS revision check

--- fsps/__init__.py
+++ fsps/__init__.py
@@ -31,14 +31,14 @@ cmd = ['svnversion', ev]
 stat, out = run_command(' '.join(cmd))
 fsps_vers = int(re.match("^([0-9])+", out[0]).group(0)) #pull out only the beginning numeric characters
 
-ACCEPTED_FSPS_REVISIONS = [135]
+ACCEPTED_FSPS_REVISIONS = [135, 136]
 accepted = ((fsps_vers in ACCEPTED_FSPS_REVISIONS) and
             (len(out[0].split(':')) == 1) ) #make sure you don't have some weird mixed version
 if not accepted:
     raise ImportError("Your FSPS revision, {0}, is not known to work with this " \
                       "version of python-fsps. Please checkout an accepted FSPS revision "\
-                      "with 'svn update -r rev_number' The accepted FSPS rev_numbers are " \
-                      "{1}".format(out[0], ACCEPTED_FSPS_REVISIONS))
+                      "with 'svn update -r rev_number' The accepted FSPS rev_numbers are: " \
+                      "{1}".format(out[0].rstrip('\n'), ACCEPTED_FSPS_REVISIONS))
     
 
 

866952005d8eb1f3d30ae4889d833ceff0254b8a
Alan Pearl <alanpearl13@gmail.com>
2019-11-01 15:42:03 -0400

    Revert "The StellarPopulation.get_spectrum method will now return a copy of self.wavelengths so that it is not modified (e.g. in case the spectrum is shifted from rest-frame with 'wavelengths *= 1+z')"
    This reverts commit 902e85b025fe7f2e3f6cb3ab58e333ebfa263296.

--- fsps/fsps.py
+++ fsps/fsps.py
@@ -581,7 +581,7 @@ class StellarPopulation(object):
         if self.params.dirty:
             self._compute_csp()
 
-        wavegrid = self.wavelengths.copy()
+        wavegrid = self.wavelengths
         if peraa:
             factor = 3e18 / wavegrid ** 2
 

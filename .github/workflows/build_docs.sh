630b90a9de1a0890330d10c145e1a9b7ba2027bc
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2021-02-15 20:51:02 -0500

    Drafting new release (#140)
    * adding release workflow
    
    * formatting in tests workflow
    
    * try brew installing gfortran on mac
    
    * brew linking
    
    * link - don't re-install
    
    * starting to handle stable docs
    
    * test docs on PR
    
    * fixing typo
    
    * testing versioned docs
    
    * I'm bad at computers
    
    * only copy stable docs on release

--- .github/workflows/build_docs.sh
+++ .github/workflows/build_docs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -l
 set -e
 
+echo "Stable: $STABLE"
+VERSION=`python -c 'import fsps;print(fsps.__version__)'`
+echo "Version: $VERSION"
+
 # Build the docs
 cd docs
 make dirhtml
@@ -11,9 +15,15 @@ cd _output
 rm -rf latest
 mkdir -p latest
 cp -r ../_build/dirhtml/* latest/
+git add latest
+
+if [ "$STABLE" = "true" ]; then
+    rm -rf $VERSION
+    cp -r latest $VERSION
+    git add $VERSION
+fi
 
 # Push the results to GitHub
-git add latest
 if git -c user.name='gh-actions' -c user.email='gh-actions' commit -m "Updated docs [ci skip]"; then
     git push --force https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY gh-pages
 else

e0817f6072e2ed0cfe2e8a4f3f1e007504de2962
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2021-02-28 16:43:03 -0500

    clean up feature_demo.py

--- demos/feature_demo.py
+++ demos/feature_demo.py
@@ -1,7 +1,12 @@
-# This script creates a set of PDFs that illustrate the effect on the SED of
-# successively turning on various options or changing the value of some
-# variables.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+"""feature_demo.py - This script creates a set of PDFs that illustrate the
+effect on the SED of successively turning on various options or changing the
+value of some variables.
+"""
+
+import os
 import numpy as np
 import matplotlib.pyplot as pl
 from matplotlib.backends.backend_pdf import PdfPages
@@ -23,13 +28,14 @@ def prettify(fig, ax, label=None):
     ax.set_xscale('log')
     ax.set_ylim(0.01, 2)
     #ax.set_yscale('log')
-    ax.set_xlabel('rest-frame $\lambda$ ($\AA$)', fontsize=20)
-    ax.set_ylabel('$\lambda \, f_\lambda$', fontsize=20)
+    ax.set_xlabel(r'rest-frame $\lambda$ ($\AA$)', fontsize=20)
+    ax.set_ylabel(r'$\lambda \, f_\lambda$', fontsize=20)
     ax.tick_params(axis='both', which='major', labelsize=16)
     if label is not None:
         ax.text(0.63, 0.85, label, transform=ax.transAxes, fontsize=16)
-    
+
     return fig, ax
+
 
 if __name__ == "__main__":
 
@@ -42,75 +48,76 @@ if __name__ == "__main__":
     pl.rc('ytick', right=True)
 
     sps = fsps.StellarPopulation(zcontinuous=1)
-    pdf = PdfPages('features.pdf')
+    os.makedirs("./figures", exist_ok=True)
+    pdf = PdfPages('./figures/features.pdf')
 
     # Basic spectrum
     sps.params['sfh'] = 4
     sps.params['tau'] = 5.0
     sps.params['logzsol'] = 0.0
-    sps.params['dust_type'] = 4 # kriek and Conroy
-    sps.params['imf_type'] = 2  #kroupa
+    sps.params['dust_type'] = 4  # kriek and Conroy
+    sps.params['imf_type'] = 2  # kroupa
     sps.params['imf3'] = 2.3
     fig, ax, spec = makefig(sps)
-    fig, ax = prettify(fig, ax, label="$\\tau=5$, Age$=13.7$,\n$\log Z/Z_\odot=0.0$")
+    fig, ax = prettify(fig, ax, label=r"$\tau=5$, Age$=13.7$,\n$\log Z/Z_\odot=0.0$")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     # change IMF
     sps.params['imf3'] = 2.5
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="IMF slope")
+    fig, ax = prettify(fig, ax, label=r"IMF slope")
     pdf.savefig(fig)
-    
+
     # Attenuate
     sps.params['add_dust_emission'] = False
     sps.params['dust2'] = 0.2
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="Dust Attenuation")
+    fig, ax = prettify(fig, ax, label=r"Dust Attenuation")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     # Dust emission
     sps.params['add_dust_emission'] = True
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="Dust Emission")
+    fig, ax = prettify(fig, ax, label=r"Dust Emission")
     pdf.savefig(fig)
     pl.close(fig)
 
     # Dust temperature
     sps.params['duste_umin'] = 10
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="Dust SED\n(Draine)")
+    fig, ax = prettify(fig, ax, label=r"Dust SED\n(Draine)")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     # AGN emission
     sps.params['fagn'] = 0.3
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="AGN dust\n(Nenkova)")
+    fig, ax = prettify(fig, ax, label=r"AGN dust\n(Nenkova)")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     # Nebular emission
     sps.params['add_neb_emission'] = True
     sps.params['gas_logu'] = -3.5
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="Neb. emission\n(Byler)")
+    fig, ax = prettify(fig, ax, label=r"Neb. emission\n(Byler)")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     # change logu
     sps.params['gas_logu'] = -1.0
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="Change U$_{neb}$")
+    fig, ax = prettify(fig, ax, label=r"Change U$_{neb}$")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     # change logz
     sps.params['logzsol'] = -0.5
     sps.params['gas_logz'] = -0.5
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="$\log Z/Z_\odot=-0.5$")
+    fig, ax = prettify(fig, ax, label=r"$\log Z/Z_\odot=-0.5$")
     pdf.savefig(fig)
     pl.close(fig)
 
@@ -118,9 +125,8 @@ if __name__ == "__main__":
     sps.params['zred'] = 6.0
     sps.params['add_igm_absorption'] = True
     fig, ax, spec = makefig(sps, oldspec=spec)
-    fig, ax = prettify(fig, ax, label="IGM attenuation\n(Madau, $z=6$)")
+    fig, ax = prettify(fig, ax, label=r"IGM attenuation\n(Madau, $z=6$)")
     pdf.savefig(fig)
     pl.close(fig)
-    
+
     pdf.close()
-    

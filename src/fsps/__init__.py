7484789df1ce2bd496b1af75fbacefa476e160cd
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2023-07-31 20:06:08 -0400

    Add __version__ attribute to module (#220)

--- src/fsps/__init__.py
+++ src/fsps/__init__.py
@@ -6,13 +6,8 @@ check_sps_home()
 del check_sps_home
 # End check
 
-from fsps.filters import (
-    find_filter as find_filter,
-)
-from fsps.filters import (
-    get_filter as get_filter,
-)
-from fsps.filters import (
-    list_filters as list_filters,
-)
+from fsps.filters import find_filter as find_filter
+from fsps.filters import get_filter as get_filter
+from fsps.filters import list_filters as list_filters
 from fsps.fsps import StellarPopulation as StellarPopulation
+from fsps.fsps_version import __version__ as __version__

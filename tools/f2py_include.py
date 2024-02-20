dad1a9dfd80a08514ef0cb8559a0115d4fdc4ec0
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2024-02-20 15:47:25 -0500

    Fix include directory on Windows

--- tools/f2py_include.py
+++ tools/f2py_include.py
@@ -3,8 +3,11 @@
 import numpy.f2py
 
 try:
-    print(numpy.f2py.get_include())
+    incl_dir = numpy.f2py.get_include()
+
 except AttributeError:
     import os
 
-    print(os.path.join(os.path.dirname(numpy.f2py.__file__), "src"))
+    incl_dir = os.path.join(os.path.dirname(numpy.f2py.__file__), "src")
+
+print(os.path.normpath(incl_dir))

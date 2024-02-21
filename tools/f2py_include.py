768d3df2dbd604e6120905b535f4cb447453c0e1
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2024-02-21 11:26:43 -0500

    Fixing import typo

--- tools/f2py_include.py
+++ tools/f2py_include.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import numpy.f2py
 
 try:
     incl_dir = numpy.f2py.get_include()
-
 except AttributeError:
-    import os
-
     incl_dir = os.path.join(os.path.dirname(numpy.f2py.__file__), "src")
 
 print(os.path.normpath(incl_dir))

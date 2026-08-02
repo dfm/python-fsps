26e8c519ac875d3f813f799a8bbbe9858ddceb87
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2026-08-02 17:18:49 -0400

    fix options test now that BaSeL is gone and C3K_LR is default.

--- tests/options.py
+++ tests/options.py
@@ -2,4 +2,4 @@ import fsps
 
 sps = fsps.StellarPopulation()
 assert sps.libraries[0] == b"pdva", "Incorrect isochrone library"
-assert sps.libraries[1] == b"basel", "Incorrect spectral library"
+assert sps.libraries[1] == b"miles", "Incorrect spectral library"
